0accc464b07e520db58f045c2d4c39c62d8aeac3
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-11-29 09:57:05 +0700

    :construction: setup styleguidist

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   ignore: [
     '**/Components/Header/**/*.js',
     '**/Components/Footer/**/*.js',
     '**/Components/Navigation/**/*.js'
-  ]
+  ],
+  template: './src/rsg-templates/index.html'
 }
