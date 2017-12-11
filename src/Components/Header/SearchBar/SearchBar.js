7dee4fef3042888782eeb7fb527765b10ae1f42f
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2017-12-11 10:47:03 +0700

    :recycle: using Downshift instead of DropdownManager

--- src/Components/Header/SearchBar/SearchBar.js
+++ src/Components/Header/SearchBar/SearchBar.js
@@ -1,52 +1,56 @@
 // @flow
 
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import cx from 'classnames'
-
-import { DropdownManager } from 'Components/Dropdown'
+import Downshift from 'downshift'
 
 class SearchBar extends Component<{}> {
   render() {
     return (
       <div className="item-fullfill mr-lg">
         <div className="form">
-          <div className="form__dropdown">
-            <input
-              type="text"
-              className="form__control"
-              placeholder="Search for products..."
-            />
-            <DropdownManager
-              render={({ isShow, toggle }) => (
-                <Fragment>
-                  <button
-                    className="form__dropdown-select"
-                    type="button"
-                    onClick={toggle}
-                  >
-                    All categories{' '}
-                    <i
-                      className={cx('fa', {
-                        'fa-caret-down': !isShow,
-                        'fa-caret-up': isShow
-                      })}
-                    />
-                  </button>
-                  <ul
-                    className={cx('form__dropdown-list', {
-                      show: isShow
+          <Downshift
+            render={({ isOpen, toggleMenu, getRootProps, closeMenu }) => (
+              <div
+                className="form__dropdown"
+                onKeyDown={e => {
+                  if (e.key === 'Escape') {
+                    closeMenu()
+                  }
+                }}
+              >
+                <input
+                  type="text"
+                  className="form__control"
+                  placeholder="Search for products..."
+                />
+                <button
+                  className="form__dropdown-select"
+                  type="button"
+                  onClick={toggleMenu}
+                >
+                  All categories{' '}
+                  <i
+                    className={cx('fa', {
+                      'fa-caret-down': !isOpen,
+                      'fa-caret-up': isOpen
                     })}
-                  >
-                    <li className="form__dropdown-item">Swimming Shorts</li>
-                    <li className="form__dropdown-item">T-Shirt</li>
-                    <li className="form__dropdown-item">Sweatshirt</li>
-                    <li className="form__dropdown-item">Tracksuit</li>
-                    <li className="form__dropdown-item">Corset</li>
-                  </ul>
-                </Fragment>
-              )}
-            />
-          </div>
+                  />
+                </button>
+                <ul
+                  className={cx('form__dropdown-list', {
+                    show: isOpen
+                  })}
+                >
+                  <li className="form__dropdown-item">Swimming Shorts</li>
+                  <li className="form__dropdown-item">T-Shirt</li>
+                  <li className="form__dropdown-item">Sweatshirt</li>
+                  <li className="form__dropdown-item">Tracksuit</li>
+                  <li className="form__dropdown-item">Corset</li>
+                </ul>
+              </div>
+            )}
+          />
         </div>
       </div>
     )
